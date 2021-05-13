9027ac1a4b911945ee169e4fd1cf010e43a31568
krcurtis <krcurtis@fhcrc.org>
2021-05-12 18:48:15 -0700

    update to fix error in checking package

--- R/interface.r
+++ R/interface.r
@@ -1,11 +1,14 @@
 ################################################################################
 ### Handle interface to command line app
 
+
 library("rjson")
 
+
+#' @export
 map_markers <- function(marker_profile_text) {
   result <- system2(c("cell-ontology-mapping", "classify", "--json", marker_profile_text), stdout=TRUE)
-  json_result <- fromJSON(result)
+  json_result <- rjson::fromJSON(result)
   if ("error_message" %in% names(json_result)) {
     stop(json_result$error_message)
   }
